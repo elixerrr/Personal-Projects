c32d631ee747670fc939567dfa6e8f3cee0c4eb8
elixerrr <154729841+elixerrr@users.noreply.github.com>
2025-07-18 17:47:58 +0530

    Update RockPaperScissors.py
    fixed bugs and made it more compact

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -1,52 +1,31 @@
 import random
-test = True
+i=0
+q=0
 question = "Yes"
-i = 0 
-q= 0
 while question.lower() == "yes" or question.lower() == "y":
-    choice2 = ["Rock", "Paper", "Scissors"]
-    randochoice2= random.choice(choice2)
-    random3 = "It's a draw!"
-    random1 = "You win :)"
-    random2 = "You lose :("
+    choice2 = ["Rock", "Paper", "Scissor"]
+    move = random.choice(choice2)
+    first = "It's a draw!"
+    second = "You win :)"
+    third = "You lose :("
     phrase= "The opponent chose "
-    if test == True:
-        i = i + 1
-    if test == False:
-        q = q + 1
-    else:
-        i==i and q==q
-    haha= str(i)
-    haha2= str(q)
-    haha3=  haha + "|" + haha2
-    choice = input("What do you want to put out?(Rock/Paper/Scissors)")
-    if choice.lower() == "rock" and randochoice2.lower() == "rock":
-        print(phrase + randochoice2 + ". " + random3)
-    if choice.lower() == "rock" and randochoice2.lower() == "paper":
-        test= False        
-        print(phrase + randochoice2 + ". " + random2)
-    if choice.lower() == "rock" and randochoice2.lower() == "scissors":
-        test= True
-        print(phrase + randochoice2 + ". " + random1)
-    if choice.lower() == "paper" and randochoice2.lower() == "paper":
-        print(phrase + randochoice2 + ". " + random3)
-    if choice.lower() == "paper" and randochoice2.lower() == "rock":
-        test= True
-        print(phrase + randochoice2 + ". " + random1)
-    if choice.lower() == "paper" and randochoice2.lower() == "scissors":
-        test= False
-        print(phrase + randochoice2 + ". " + random2)
-    if choice.lower() == "scissors" and randochoice2.lower() == "scissors":
-        print(phrase + randochoice2 + ". " + random3)
-    if choice.lower() == "scissors" and randochoice2.lower() == "rock":
-        test= False
-        print(phrase + randochoice2 + ". " + random2)
-    if choice.lower() == "scissors" and randochoice2.lower() == "paper":
-        test= True
-        print(phrase + randochoice2 + ". " + random1)
-    print("The current score is " + haha3)
+    choice = input ("What do you want to put out?(Rock/Paper/Scissor)")
+    if move== choice:
+        print(first)
+    if (choice== "Rock" and move=="Paper") or (choice== "Paper" and move=="Scissor") or (choice=="Scissor" and move=="Rock"):
+        q= q+1
+        player= str(i)
+        computer= str(q)
+        score= (player + "|" + computer)
+        print(third+ phrase + move + "." + "The current score is " + score + ".")
+    if (choice== "Paper" and move=="Rock") or (choice== "Scissor"and move=="Paper") or (choice=="Rock" and move=="Scissor"):
+        i=i+1
+        player= str(i)
+        computer= str(q)
+        score= (player + "|" + computer)
+        print(second+ phrase + move + "." + "The current score is " + score + ".")    
     question= input("Do you wish to play more?(Yes/No)")
-    if question.lower()=="No":
-        print("Thanks for playing! " + "Your score is " + haha3)
-        exit()
+    if question.lower()== "no":
+        print("Thanks for playing!")
+        break
 
